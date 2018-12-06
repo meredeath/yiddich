8e22e8c955cfea9d9061a6350bb30e4d88a6cd71
Meredith <mhu2@stuy.edu>
2018-12-06 14:47:16 -0500

    hw finally fixed! Memory is able to be deleted now (used strcmp incorrectly

--- yiddich.c
+++ yiddich.c
@@ -17,12 +17,37 @@ int main(){
 
   if(*data){
     printf("current contents: %s\n", data);
-  }else printf("nothing in data; newly created");
+  }else printf("nothing in data; newly created\n");
 
   char buf[200];
   printf("Do you want to modify the shared memory? (say y or n)\n");
   fgets(buf, 200, stdin);
-  strcpy(data, buf);
+  if(!strcmp(buf,"y\n")){
+    fflush(stdin);
+    char newstuff[200];
+    printf("what do you want in the memory?\n");
+    fgets(newstuff, 200, stdin);
+    strcpy(data, newstuff);
+    printf("mem successfully modified\n");
+  }else if(!strcmp(buf,"n\n")){
+    printf("mem not modified\n");
+  }else{
+    printf("y or n was not entered\n");
+  }
+
+  char deletes[5];
+  printf("Do you want to delete the segment? (y or n)\n");
+  fflush(stdin);
+  fgets(deletes, 200, stdin);
+  if(!strcmp(deletes,"y\n")){
+    shmdt(data);
+    shmctl(shmid, IPC_RMID, NULL);
+    printf("mem successfully deleted\n");
+  }else if(!strcmp(deletes,"n\n")){
+    printf("mem not deleted\n");
+  }else{
+    printf("y or n was not entered\n");
+  }
   
   return 0;
 }
